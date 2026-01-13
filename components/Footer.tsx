423ccc48b0fac2025103b2bed41a726733f58af9
Sourav Suman <42172921+souravsuman1999@users.noreply.github.com>
2026-01-13 12:07:17 +0530

    H

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -89,12 +89,7 @@ export default function Footer() {
                 </svg>
                 <span>New Delhi, India</span>
               </li>
-              <li className="footer-contact-item">
-                <svg width="16" height="16" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
-                </svg>
-                <span>contact@aimission.co</span>
-              </li>
+  
               {/* <li className="footer-contact-item">
                 <svg width="16" height="16" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M20 15.5c-1.25 0-2.45-.2-3.57-.57-.35-.11-.74-.03-1.02.24l-2.2 2.2c-2.83-1.44-5.15-3.75-6.59-6.59l2.2-2.21c.28-.26.36-.65.25-1C8.7 6.45 8.5 5.25 8.5 4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1 0 9.39 7.61 17 17 17 .55 0 1-.45 1-1v-3.5c0-.55-.45-1-1-1z" />
